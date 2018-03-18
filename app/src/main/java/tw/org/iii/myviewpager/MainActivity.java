cf1ecb9a83b15a36ec4a558196835069a1abe818
Brad <brad@brad.tw>
2018-03-18 15:47:02 +0800

    v2

--- app/src/main/java/tw/org/iii/myviewpager/MainActivity.java
+++ app/src/main/java/tw/org/iii/myviewpager/MainActivity.java
@@ -4,21 +4,25 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ViewFlipper;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ViewPager pager;
     private ArrayList<View> views;
+    private ViewFlipper flipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //flipper = findViewById(R.id.flipper);
         pager = findViewById(R.id.pager);
         initPager();
     }
@@ -27,14 +31,51 @@ public class MainActivity extends AppCompatActivity {
         views = new ArrayList<>();
 
         LayoutInflater inflater = LayoutInflater.from(this);
+        View page0 = inflater.inflate(R.layout.page0, null);
         View page1 = inflater.inflate(R.layout.page1, null);
         View page2 = inflater.inflate(R.layout.page2, null);
         View page3 = inflater.inflate(R.layout.page3, null);
-
+        View page4 = inflater.inflate(R.layout.page4, null);
+        views.add(page0);
         views.add(page1);views.add(page2);views.add(page3);
+        views.add(page4);
+
+        flipper = page2.findViewById(R.id.flipper);
+        View f0 = flipper.getChildAt(0);
+        View f1 = flipper.getChildAt(1);
+        View f2 = flipper.getChildAt(2);
+        FlipperOnClick flipperOnClick = new FlipperOnClick();
+        f0.setOnClickListener(flipperOnClick);
+        f1.setOnClickListener(flipperOnClick);
+        f2.setOnClickListener(flipperOnClick);
 
 
         pager.setAdapter(new MyPagerAdapter());
+        pager.setOnPageChangeListener(new MyPagerChangeListener());
+        pager.setCurrentItem(1);
+
+    }
+
+    private class FlipperOnClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            flipper.showNext();
+        }
+    }
+
+
+    private class MyPagerChangeListener extends ViewPager.SimpleOnPageChangeListener {
+
+        @Override
+        public void onPageSelected(int position) {
+            super.onPageSelected(position);
+            Log.v("brad", "now : " + position);
+            if (position == 0){
+                pager.setCurrentItem(1);
+            }else if (position == 4){
+                pager.setCurrentItem(3);
+            }
+        }
     }
 
     private class MyPagerAdapter extends PagerAdapter {
